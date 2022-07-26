98d39eef3b1f31dd89902eef99e84095482d918c
Wilton Guilherme <wiltoninfra@gmail.com>
2022-07-26 12:04:00 -0300

    Formart file

--- src/shell/auth.sh
+++ src/shell/auth.sh
@@ -14,20 +14,29 @@ AWS_ACCESS_KEY=$AWS_ACCESS_KEY
 AWS_SECRET_KEY=$AWS_SECRET_KEY
 
 validate_vars() {
-if [ -z ${AWS_REGION} ]; then { echo "AWS_REGION is var not defined"; exit 1; } fi 
-if [ -z ${AWS_ACCESS_KEY} ]; then { echo "AWS_ACCESS_KEY is var not defined"; exit 1; } fi 
-if [ -z ${AWS_SECRET_KEY} ]; then { echo "AWS_SECRET_KEY is var not defined"; exit 1; } fi 
+    if [ -z ${AWS_REGION} ]; then {
+        echo "AWS_REGION is var not defined"
+        exit 1
+    }; fi
+    if [ -z ${AWS_ACCESS_KEY} ]; then {
+        echo "AWS_ACCESS_KEY is var not defined"
+        exit 1
+    }; fi
+    if [ -z ${AWS_SECRET_KEY} ]; then {
+        echo "AWS_SECRET_KEY is var not defined"
+        exit 1
+    }; fi
 }
 
-verify_files () {
-    if [ ! -d /root/.aws ]
-    then mkdir -p /root/.aws
+verify_files() {
+    if [ ! -d /root/.aws ]; then
+        mkdir -p /root/.aws
     fi
 }
 
-aws_config () {
-    sed -e "s;%AWS_REGION%;$AWS_REGION;g" /srv/aws-config.tmpl > /root/.aws/config
-    sed -e "s;%AWS_ACCESS_KEY%;$AWS_ACCESS_KEY;g" -e "s;%AWS_SECRET_KEY%;$AWS_SECRET_KEY;g" /srv/aws-credentials.tmpl > /root/.aws/credentials
+aws_config() {
+    sed -e "s;%AWS_REGION%;$AWS_REGION;g" /srv/aws-config.tmpl >/root/.aws/config
+    sed -e "s;%AWS_ACCESS_KEY%;$AWS_ACCESS_KEY;g" -e "s;%AWS_SECRET_KEY%;$AWS_SECRET_KEY;g" /srv/aws-credentials.tmpl >/root/.aws/credentials
 }
 
 validate_vars
